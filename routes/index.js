aaef76cbcf679d644eae156df3313842a7241147
jmccrae <johnmccraejnr@hotmail.com>
2016-08-17 22:11:15 +0100

    fix sloppy errors commit

---
+++ routes/index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+// Mongoose import
+var mongoose = require('mongoose');
+
+// Mongoose connection to MongoDB
+mongoose.connect('mongodb://localhost/leaflet_map', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+// Mongoose Schema definition
+var Schema = mongoose.Schema;
+var JsonSchema = new Schema({
+    name: String,
+    type: Schema.Types.Mixed
+});
+ 
+// Mongoose Model definition
+var Json = mongoose.model('JString', JsonSchema, 'layercollection');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET json data. */
+router.get('/mapjson/:name', function (req, res) {
+    if (req.params.name) {
+        Json.findOne({ name: req.params.name },{}, function (err, docs) {
+            res.json(docs);
+        });
+    }
+});
+
+/* GET layers json data. */
+router.get('/maplayers', function (req, res) {
+    Json.find({},{'name': 1}, function (err, docs) {
+        res.json(docs);
+    });
+});
+
+/* GET Map page. */
+router.get('/map', function(req,res) {
+    var db = req.db;
+    Json.find({},{}, function(e,docs){
+        res.render('map', {
+            "jmap" : docs,
+            lat : 40.78854,
+            lng : -73.96374
+        });
+    });
+});
+
+module.exports = router;
